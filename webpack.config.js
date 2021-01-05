f2dd1bb00c5e89b0ca27ad22924fa24b13e2a5d7
kevin <kevinfrom@live.dk>
2021-01-05 19:37:08 +0100

    made webpack config export a function to allow for more fine control

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 const path = require('path')
 
-module.exports = {
-    devtool: 'eval-source-map',
-    entry: './src/index.ts',
-    module: {
-        rules: [{
-            test: /\.ts$/i,
-            use: 'ts-loader',
-            include: [path.resolve(__dirname, 'src')]
-        }, ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 8080,
-        allowedHosts: [
-            'localhost',
-            '127.0.0.1'
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+module.exports = (env, argv) => {
+    const isProduction = argv.mode.toString().toLowerCase() === 'production';
+
+    return {
+        devtool: isProduction ? false : 'eval-source-map',
+        entry: './src/index.ts',
+        module: {
+            rules: [{
+                test: /\.ts$/i,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')]
+            }, ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 8080,
+            allowedHosts: [
+                'localhost',
+                '127.0.0.1'
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        }
     }
 }
